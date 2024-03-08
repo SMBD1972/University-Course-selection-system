111b741e57716a8e02d54edaa7c4eff1df830d80
SMBD1972 <samanbadiee72@gmail.com>
2024-03-08 19:16:28 +0330

    MakePackageAndClasses

---
+++ src/Course/Computer_Engineering.java
@@ -0,0 +1,4 @@
+package Course;
+
+public class Computer_Engineering {
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import Course.Electrical_Engineering;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    } }
+
+
